5d5fb7c2eddc944655d0685d8b0ee240b87b99f1
dauni6 <45280927+dauni6@users.noreply.github.com>
2021-12-06 22:16:03 +0900

    3주차 3차시 - Jetpack Compose Navigation
    1. NavHost()를 통하여 네비게이션 그래프를 정의할 수 있다.
    
    2. NavHost() { /* NavGraphBuilder */} 를 통하여 NavHost가 이동할 수 있는 목적지들을을 받아들일 준비를 하고  이를 NavGraphBuilder가 수행하게 된다.
    
    3. NavHraphBuilder가 수행할 수 있도록 composable(RallyScreen.Overview.name) {
    } 을 통하여 목적지를 정할 수 있다.
    
    4. 인자를 추가할 경우 명명된 인자는 {} 중괄호로 묶어서 사용해야하며
    변수이름을 이스케이핑하기 위하여 $기호를 사용한다.
    
    5. Navigation은 백스택을 유지하고 현재 백스택 항목을 state로 제공해준다.
    
    6. Deep Link를 사용하기 위해 매니페스트에 intent-filter를 추가하고 adb shell로 테스트한다.
    
    7. navigation 테스트를 시도하였다.

---
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -0,0 +1,82 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.fail
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class RallyNavHostTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: NavHostController
+
+    @Before
+    fun setupRallyNavHost() {
+        composeTestRule.setContent {
+            navController = rememberNavController()
+            RallyNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun rallyNavHost() {
+        composeTestRule.setContent {
+            navController = rememberNavController()
+            RallyNavHost(navController = navController)
+        }
+//        fail()
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_navigateToAllAccounts_viaUI() {
+        composeTestRule
+            .onNodeWithContentDescription("All Accounts")
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
+            .assertIsDisplayed()
+    }
+
+    // UI, navController를 통한 테스트
+    @Test
+    fun rallyNavHost_navigateToBills_viaUI() {
+        // "All Bills"를 클릭할 때
+        composeTestRule.onNodeWithContentDescription("All Bills").apply {
+            performScrollTo()
+            performClick()
+        }
+        // 현재 경로가 "Bills"인지 확인
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "Bills")
+    }
+
+    @Test
+    fun rallyNavHost_navigateToAllAccounts_callingNavigate() {
+        // ui스레드에서  navController.navigate를 호출해야 하는데 이를 위해 runBli
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                navController.navigate(RallyScreen.Accounts.name)
+            }
+        }
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
+            .assertIsDisplayed()
+    }
+
+
+}
--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -22,13 +22,27 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.accounts.SingleAccountBody
+import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -48,23 +62,95 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState() // 백스택을 사용하기 위한 state
+        val currentScreen = RallyScreen.fromRoute(
+            backstackEntry.value?.destination?.route
+        )
+
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen ->
+                        navController.navigate(screen.name) // tab 클릭시 navigating
+                    },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.content(
-                    onScreenChange = { screen ->
-                        currentScreen = RallyScreen.valueOf(screen)
-                    }
-                )
-            }
+            RallyNavHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
 }
+
+@Composable
+fun RallyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost( // NavHost를 정의했으므로 네비게이션 그래프를 정의할 수 있다.
+        navController = navController,
+        startDestination = RallyScreen.Overview.name, // startDestination이 반드시 필요
+        modifier = modifier
+    ) {
+        // 여기서 NavHost가 이동할 수 있는 목적지들은 다른 목적지들을 수용할 준비가 되었다.
+        // 이를 NavGraphBuilder를 통해 수행한다.
+
+        // OverviewBody로 가는 목적지
+        composable(RallyScreen.Overview.name) {
+            OverviewBody(
+                onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+                onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
+                onAccountClick = { name ->
+                    navigateToSingleAccount(navController, name)
+                }
+            )
+        }
+        // AccountsBody로 가는 목적지
+        composable(RallyScreen.Accounts.name) {
+            AccountsBody(accounts = UserData.accounts) { name ->
+                navigateToSingleAccount(
+                    navController = navController,
+                    accountName = name
+                )
+            }
+        }
+        // BillsBody로 가는 목적지
+        composable(RallyScreen.Bills.name) {
+            BillsBody(bills = UserData.bills)
+        }
+
+        val accountsName = RallyScreen.Accounts.name
+        // SingleAccountBody로 가는 새로운 목적지 추가
+        composable(
+            "$accountsName/{name}", // 명명된 인자는 {} 중괄호로 묶여서 내부로 제공된다. 또한 변수이름을 이스케이핑 하기위해 $ 기호를 사용한다.
+            arguments = listOf(
+                navArgument("name") {
+                    // Make argument type safe
+                    type = NavType.StringType
+                }
+            ),
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "rally://$accountsName/{name}"
+            })
+        ) { entry -> // NavBackStackEntry의 인자들중 "name"을 검색한다.
+            val accountName = entry.arguments?.getString("name")
+            // UserData에서 일치하는 첫번째 이름을 찾는다.
+            val account = UserData.getAccount(accountName)
+            // 계좌를 SingleAccountBody로 전달한다.
+            SingleAccountBody(account = account)
+        }
+    }
+}
+
+private fun navigateToSingleAccount(
+    navController: NavHostController,
+    accountName: String
+) {
+    navController.navigate("${RallyScreen.Accounts.name}/$accountName")
+}
+
